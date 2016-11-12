4f90491b9d44e9c106477dd8659e9c71b5c13355
biraj01 <birajdhngl01@gmail.com>
2016-11-12 23:03:55 +0100

    read and write character

--- src/main/java/proxy/POP3Proxy.java
+++ src/main/java/proxy/POP3Proxy.java
@@ -1,5 +1,6 @@
 package proxy;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -76,9 +77,10 @@ public class POP3Proxy {
 	class POP3ServerThread extends Thread {
 		private boolean serving = true;
 		private Socket socket;
-		private STATE state = STATE.AUTH; 
+		private STATE state = STATE.AUTH;
 		OutputStreamWriter out;
 		InputStreamReader in;
+		BufferedReader reader;
 
 		POP3ServerThread(Socket socket) {
 			this.socket = socket;
@@ -90,9 +92,9 @@ public class POP3Proxy {
 				out = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
 				write("+OK server is ready");
 				in = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
-
+				reader = new BufferedReader(in);
 				while (serving) {
-					if (state == STATE.AUTH){
+					if (state == STATE.AUTH) {
 						authenticate();
 					} // Transaction State
 					else if (state == STATE.TRANS) {
@@ -109,15 +111,22 @@ public class POP3Proxy {
 
 		}
 
-		private void authenticate() {
-			String line = read();
-			System.out.println(line + line.length());
+		private void authenticate() throws IOException {
+			// String line = read();
+			String line = reader.readLine();
+			System.out.println(line + " " + line.length());
 			if (line.startsWith("CAPA") || line.startsWith("AUTH")) {
+				System.out.println("In capa");
 				handleCapa();
 			} else if (line.startsWith("USER ")) {
-				handleUser(line);
-				line = read();
-				handlePassword(line);
+				System.out.println("in User");
+				boolean userCorrect = handleUser(line);
+				if (userCorrect) {
+					// String line = read();
+					line = reader.readLine();
+					System.out.println(line.length() + " so long");
+					handlePassword(line);
+				}
 			} else if (line.startsWith("QUIT ")) {
 				handleQuit();
 			} else {
@@ -129,33 +138,16 @@ public class POP3Proxy {
 			write("-ERR CAPA ist shit");
 		}
 
-		private void handleUnknownCommand() {
-			write("-ERR Unknown Command");
-		}
-
-		private String read() {
-			char[] cbuff = new char[255];
-			String s = "";
-			try {
-				in.read(cbuff);
-				s = String.valueOf(cbuff);
-			} catch (IOException e) {
-
+		private synchronized boolean handleUser(String line) {
+			if (line.substring(5).equals(username)) {
+				write("+OK Please enter password");
+				System.out.println("User name authentification successful");
+				return true;
+			} else {
+				write("-ERR Wrong Username");
+				return false;
 			}
-			return s;
-		}
 
-		private void write(String line) {
-			line = line + "\r\n";
-			try {
-				out.write(line);
-				out.flush();
-			} catch (IOException e) {
-			}
-		}
-
-		private synchronized void handleQuit() {
-			write("-ERR mailbox closing");
 		}
 
 		private synchronized void handlePassword(String line) {
@@ -168,73 +160,48 @@ public class POP3Proxy {
 
 		}
 
-		private synchronized void handleUser(String line) {
-			System.out.println(line.substring(5) + " " + line.length());
-			if (line.substring(5).equals(username)) {
-				write("+OK Please enter password");
-			} else {
-				write("-ERR Wrong Username");
-			}
+		private synchronized void handleQuit() {
+			write("-ERR mailbox closing");
+		}
 
+		private void handleUnknownCommand() {
+			write("-ERR Unknown Command");
 		}
 
 		private synchronized void transaction() throws IOException, MessagingException {
-			String line = read();
+			// String line = read();
+			String line = reader.readLine();
 			if (line.startsWith("STAT ")) {
-				// OK + nr of msg in maildrop + " " + // + size of mail drop in
-				// octets
 				handleStat();
 			} else if (line.startsWith("LIST ")) {
-				// param optional
-				// with param //ok + mail with param or error if the msg nr is
-				// not there
-				// whithout param //ok + all mail
 				handleList();
 			} else if (line.startsWith("RETR ")) {
-				// RETR msg ->msg is num of
-				// message( param required)
 				handleRetr(line);
 			} else if (line.startsWith("DELE ")) {
-				// param msg nr required
 				handleDelete(line);
 			} else if (line.startsWith("NOOP ")) {
-				// Just reply with positive response
 				handleNoop();
-			} else if (line.startsWith("RSET")) { // no param
-				// Unmark the message that are marked as deleted
+			} else if (line.startsWith("RSET")) {
 				handleReset(line);
 			} else if (line.startsWith("UIDL")) {
 				handleUIDL(line);
-				// if with paramater(msg nr)
-				// if msg nr is there OK + message nr + " " + unique id from
-				// message
-				// if msg nr is not there -ERR no such message, only 2 messages
-				// in maildrop
-				// without parameter
-				// multi line, +OK as first line and each line with message nr +
-				// " " + unique id from message
 			} else if (line.startsWith("QUIT ")) {
-				// in translation state Quit command
-				// will change the state to UPDATE
-				// delete any marked message and quit
 				handleQuitTrans();
 			}
 
 		}
 
-		private synchronized void handleQuitTrans() {
-			state = STATE.UPDATE;
-			serving = false;
-		}
-
-		private synchronized void handleNoop() {
-			write("+OK");
-		}
-
+		/*
+		 * OK + nr of msg in maildrop + " " + // + size of mail drop in octets
+		 */
 		private synchronized void handleStat() {
 			write("+OK " + messages.size() + "size of maildrop in octals");
 		}
 
+		/*
+		 * param optional with param ok + mail with param or error if the msg nr
+		 * is not there without param ok + all mail
+		 */
 		private synchronized void handleList() throws IOException {
 			if (messages.size() > 0) {
 				for (Message message : messages) {
@@ -247,6 +214,9 @@ public class POP3Proxy {
 
 		}
 
+		/*
+		 * RETR msg ->msg is num of message( param required)
+		 */
 		private synchronized void handleRetr(String line) throws IOException {
 			write("+OK ---octals");
 			int messageNr = Integer.parseInt(line.substring(6));
@@ -257,13 +227,9 @@ public class POP3Proxy {
 			}
 		}
 
-		private synchronized void handleReset(String line) throws MessagingException, IOException {
-			for (Message message : messages) {
-				message.setFlag(Flag.DELETED, false);
-			}
-			write("+ok");
-		}
-
+		/*
+		 * // param msg nr required
+		 */
 		private synchronized void handleDelete(String line) throws IOException, MessagingException {
 			boolean contains = false;
 			for (Message message : messages) {
@@ -277,6 +243,31 @@ public class POP3Proxy {
 				write("-ERR only " + messages.size() + "are avialable");
 			}
 		}
+
+		/*
+		 * Just reply with positive response
+		 */
+		private synchronized void handleNoop() {
+			write("+OK");
+		}
+
+		/*
+		 * Unmark the message that are marked as deleted no param
+		 */
+		private synchronized void handleReset(String line) throws MessagingException, IOException {
+			for (Message message : messages) {
+				message.setFlag(Flag.DELETED, false);
+			}
+			write("+ok");
+		}
+
+		/*
+		 * if with paramater(msg nr) if msg nr is there OK + message nr + " " +
+		 * unique id from message if msg nr is not there -ERR no such message,
+		 * only 2 messages in maildrop without parameter multi line, +OK as
+		 * first line and each line with message nr + " " + unique id from
+		 * message
+		 */
 
 		private synchronized void handleUIDL(String line) throws IOException {
 			if (line.length() > 5) {
@@ -296,6 +287,45 @@ public class POP3Proxy {
 			}
 
 		}
+
+		/*
+		 * in translation state Quit command will change the state to UPDATE
+		 * delete any marked message and quit
+		 */
+		private synchronized void handleQuitTrans() {
+			state = STATE.UPDATE;
+			serving = false;
+		}
+
+		/*
+		 * Read each char till a line ends
+		 */
+		private String read() {
+			int i;
+			StringBuilder sb = new StringBuilder("");
+			try {
+				sb = new StringBuilder();
+				while (0 <= (i = in.read()) && i != '\r' && i != '\n') {
+					sb.append((char) i);
+					System.out.println(sb.toString());
+				}
+
+			} catch (IOException e) {
+
+			}
+			System.out.println("end of read");
+			return sb.toString();
+		}
+
+		private void write(String line) {
+			line = line + "\r\n";
+			try {
+				out.write(line);
+				out.flush();
+			} catch (IOException e) {
+			}
+		}
+
 	}
 
 	private ArrayList<Message> receivingEmails(UserAccount user) {
